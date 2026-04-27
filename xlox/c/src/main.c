a9271df3cd1e73430dcb355f9cb2ee941fc9a8e2
Cuong Dang <cuongd@pm.me>
2026-04-27 17:29:11 -0500

    xlox: Init VM

--- xlox/c/src/main.c
+++ xlox/c/src/main.c
@@ -1,7 +1,10 @@
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
 
@@ -9,6 +12,8 @@ int main(int argc, const char *argv[]) {
   writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
